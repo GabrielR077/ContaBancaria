c5420e9f67cbbef8945ff677ccf67c84fecbab08
gabriel.rocha <gabrielribeiro2547@gmail.com>
2025-03-07 08:39:41 -0300

    Initial Upload

---
+++ ContaBancaria/src/ProjetoBanco/contaBancaria.java
@@ -0,0 +1,11 @@
+package ProjetoBanco;
+
+import java.util.Scanner;
+
+public class contaBancaria {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		scan.close();
+	}
+}
