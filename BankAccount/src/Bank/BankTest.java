da638924b3a40b0a2677bd36179dbd1589832778
gabriel.rocha <gabrielribeiro2547@gmail.com>
2025-03-07 09:04:23 -0300

    Upload2

--- BankAccount/src/Bank/BankTest.java
+++ BankAccount/src/Bank/BankTest.java
@@ -1,7 +1,11 @@
 package Bank;
 
+import java.util.Scanner;
+
 public class BankTest {
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 		
+		scan.close();
 	}
 }
