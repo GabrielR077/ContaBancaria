f0d1ac2b4159d6968866129feaad3e0974a7542c
gabriel.rocha <gabrielribeiro2547@gmail.com>
2025-03-07 10:54:37 -0300

    Versão Final

---
+++ ContaBancaria/src/ProjetoBanco/contaTerminal.java
@@ -0,0 +1,40 @@
+package ProjetoBanco;
+
+import java.util.Scanner;
+
+public class contaTerminal {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+	
+		System.out.println("===================================="
+				+ "=====================================================");
+		
+		System.out.println("Olá querido Cliente, "
+				+ "informe seu nome, Número e Agência "
+				+ "para a consulta de seu saldo!");	
+		System.out.println("=========================================="
+				+ "===============================================");
+		
+		//	Inserindo Dados
+		
+		System.out.println("Insira seu nome: ");
+		String nomeCLiente = scan.nextLine();
+		
+		
+		System.out.println("Insira o número de sua conta: ");
+		int numero = scan.nextInt();
+
+		System.out.println("Agora insira sua agência: ");
+		int agencia = scan.nextInt();
+
+		double saldo = 250.0;
+		System.out.println("Seu saldo disponível para saque é: " + saldo);
+		
+		//	Finalizando o programa
+		System.out.println("Pressione enter para sair!");
+		
+		scan.close();
+		
+		System.exit(0);
+	}
+}
